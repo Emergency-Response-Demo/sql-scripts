f424dc7426245f242cd8a65b11ef80eb12c62072
Manav Kumar <mankumar@redhat.com>
2019-03-11 16:46:46 -0400

    Database backup

---
+++ naps_emergency_response.sql
@@ -0,0 +1,247 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.10
+-- Dumped by pg_dump version 9.6.10
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: incident; Type: TABLE; Schema: public; Owner: naps
+--
+
+CREATE TABLE public.incident (
+    incident_id integer NOT NULL,
+    report_id integer,
+    gps_lat double precision,
+    gps_long double precision,
+    number_of_people integer,
+    medical_need boolean,
+    report_time timestamp without time zone
+);
+
+
+ALTER TABLE public.incident OWNER TO naps;
+
+--
+-- Name: incident_sequence; Type: SEQUENCE; Schema: public; Owner: naps
+--
+
+CREATE SEQUENCE public.incident_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.incident_sequence OWNER TO naps;
+
+--
+-- Name: incident_sequence; Type: SEQUENCE OWNED BY; Schema: public; Owner: naps
+--
+
+ALTER SEQUENCE public.incident_sequence OWNED BY public.incident.incident_id;
+
+
+--
+-- Name: mission; Type: TABLE; Schema: public; Owner: naps
+--
+
+CREATE TABLE public.mission (
+    mission_id integer NOT NULL,
+    responder_id integer,
+    last_update date,
+    current_status public.status,
+    incident_id integer
+);
+
+
+ALTER TABLE public.mission OWNER TO naps;
+
+--
+-- Name: mission_sequence; Type: SEQUENCE; Schema: public; Owner: naps
+--
+
+CREATE SEQUENCE public.mission_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.mission_sequence OWNER TO naps;
+
+--
+-- Name: mission_sequence; Type: SEQUENCE OWNED BY; Schema: public; Owner: naps
+--
+
+ALTER SEQUENCE public.mission_sequence OWNED BY public.mission.mission_id;
+
+
+--
+-- Name: reporter; Type: TABLE; Schema: public; Owner: naps
+--
+
+CREATE TABLE public.reporter (
+    reporter_id integer NOT NULL,
+    reporter_name text,
+    reporter_phone_number text
+);
+
+
+ALTER TABLE public.reporter OWNER TO naps;
+
+--
+-- Name: reporter_sequence; Type: SEQUENCE; Schema: public; Owner: naps
+--
+
+CREATE SEQUENCE public.reporter_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.reporter_sequence OWNER TO naps;
+
+--
+-- Name: reporter_sequence; Type: SEQUENCE OWNED BY; Schema: public; Owner: naps
+--
+
+ALTER SEQUENCE public.reporter_sequence OWNED BY public.reporter.reporter_id;
+
+
+--
+-- Name: responder; Type: TABLE; Schema: public; Owner: naps
+--
+
+CREATE TABLE public.responder (
+    responder_id integer NOT NULL,
+    responder_name text,
+    responder_phone_number text,
+    responder_current_gps_lat double precision,
+    responder_current_gps_long double precision,
+    boat_capacity integer,
+    has_medical_kit boolean
+);
+
+
+ALTER TABLE public.responder OWNER TO naps;
+
+--
+-- Name: responder_sequence; Type: SEQUENCE; Schema: public; Owner: naps
+--
+
+CREATE SEQUENCE public.responder_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.responder_sequence OWNER TO naps;
+
+--
+-- Name: responder_sequence; Type: SEQUENCE OWNED BY; Schema: public; Owner: naps
+--
+
+ALTER SEQUENCE public.responder_sequence OWNED BY public.responder.responder_id;
+
+
+--
+-- Name: incident incident_id; Type: DEFAULT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.incident ALTER COLUMN incident_id SET DEFAULT nextval('public.incident_sequence'::regclass);
+
+
+--
+-- Name: mission mission_id; Type: DEFAULT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.mission ALTER COLUMN mission_id SET DEFAULT nextval('public.mission_sequence'::regclass);
+
+
+--
+-- Name: reporter reporter_id; Type: DEFAULT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.reporter ALTER COLUMN reporter_id SET DEFAULT nextval('public.reporter_sequence'::regclass);
+
+
+--
+-- Name: responder responder_id; Type: DEFAULT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.responder ALTER COLUMN responder_id SET DEFAULT nextval('public.responder_sequence'::regclass);
+
+
+--
+-- Name: incident incident_pkey; Type: CONSTRAINT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.incident
+    ADD CONSTRAINT incident_pkey PRIMARY KEY (incident_id);
+
+
+--
+-- Name: mission mission_pkey; Type: CONSTRAINT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.mission
+    ADD CONSTRAINT mission_pkey PRIMARY KEY (mission_id);
+
+
+--
+-- Name: reporter reporter_pkey; Type: CONSTRAINT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.reporter
+    ADD CONSTRAINT reporter_pkey PRIMARY KEY (reporter_id);
+
+
+--
+-- Name: responder responder_pkey; Type: CONSTRAINT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.responder
+    ADD CONSTRAINT responder_pkey PRIMARY KEY (responder_id);
+
+
+--
+-- Name: incident incident_report_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.incident
+    ADD CONSTRAINT incident_report_id_fkey FOREIGN KEY (report_id) REFERENCES public.reporter(reporter_id);
+
+
+--
+-- Name: mission mission_responder_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: naps
+--
+
+ALTER TABLE ONLY public.mission
+    ADD CONSTRAINT mission_responder_id_fkey FOREIGN KEY (responder_id) REFERENCES public.reporter(reporter_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
